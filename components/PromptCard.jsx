1a0e8479fcb9030d9d9302dd145b870427493911
David Stowe <david.s.stowe@gmail.com>
2023-05-29 17:46:26 -0400

    modified

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -33,7 +33,7 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
           <Image src={post.creator.image} alt="user_image" width={40} height={40} className="rounded-full object-contain" />
 
           <div className="flex flex-col">
-            <h3 className="font-satoshi font-semibold text-gray-900">{post.creator.username}</h3>
+            <h2 className="font-satoshi font-semibold text-gray-900">{post.creator.username}</h2>
             <p className="font-inter text-sm text-gray-500">{post.creator.email}</p>
           </div>
         </div>
